6f1e5d117b8d9b86359287287c632f142ac1c71b
KKT89 <kkt89swa@gmail.com>
2025-04-22 03:49:05 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+}
