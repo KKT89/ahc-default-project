a53f14beff0e2945acb893ecaa781edef26d7bc4
KKT89 <kkt89swa@gmail.com>
2025-04-23 01:53:26 +0900

    add lib

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-typedef unsigned long long int ull;
 
-int main() {
+#include "params.cpp"
+#include "lib/heuristic-lib/timer.hpp"
+#include "lib/heuristic-lib/rnd32.hpp"
+
+int main(int argc, char *argv[]) {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
+    updateParams(argc, argv);
 }
